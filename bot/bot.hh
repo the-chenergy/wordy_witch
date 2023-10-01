066a0311aa9e8a9c9905c5cfe647c2ef6be97137
Qianlang Chen <qianlangchen@gmail.com>
2023-09-30 20:58:11 -0700

    Add hard mode checking

--- bot/bot.hh
+++ bot/bot.hh
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <algorithm>
+#include <bitset>
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <optional>
 #include <string>
 
@@ -51,14 +53,52 @@ std::string format_verdict(int verdict) {
   return tiles;
 }
 
+bool check_is_hard_mode_valid(const char* prev_guess, int prev_verdict,
+                              const char* candidate_guess) {
+  int verdict_tiles[WORD_SIZE];
+  for (int i = WORD_SIZE - 1; i >= 0; i--) {
+    verdict_tiles[i] = prev_verdict % 3;
+    prev_verdict /= 3;
+  }
+
+  int letter_counts[ALPHABET_SIZE] = {};
+  for (int i = 0; i < WORD_SIZE; i++) {
+    if (verdict_tiles[i] != VERDICT_VALUE_BLACK) {
+      letter_counts[prev_guess[i]]++;
+      if (verdict_tiles[i] == VERDICT_VALUE_GREEN) {
+        if (candidate_guess[i] != prev_guess[i]) {
+          return false;
+        }
+      }
+    }
+    letter_counts[candidate_guess[i]]--;
+  }
+  for (int i = 0; i < WORD_SIZE; i++) {
+    if (letter_counts[prev_guess[i]] > 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 constexpr int MAX_BANK_SIZE = 1 << 14;
+constexpr int NUM_VERDICTS = 243;
 
 struct Bank {
   char words[MAX_BANK_SIZE][WORD_SIZE + 1];
   int num_words;
   int num_targets;
 
+  /* `verdict[guess][target]` => `judge(guess, target)` */
   uint8_t verdicts[MAX_BANK_SIZE][MAX_BANK_SIZE];
+  /*
+    `hard_mode_valid_candidates[prev_guess][candidate_guess_verdict][prev_verdict]`
+    => under hard mode, whether some candidate word with verdict
+    `judge(prev_guess, candidate_word)` may be used as the next guess if
+    prev_verdict (i.e. `judge(prev_guess, target)`) was given
+  */
+  std::bitset<NUM_VERDICTS> hard_mode_valid_candidates[MAX_BANK_SIZE]
+                                                      [NUM_VERDICTS];
 };
 
 enum struct BankGuessesInclusion : int {
@@ -109,9 +149,31 @@ void load_bank(Bank& out_bank, std::filesystem::path dict_path,
   read_bank();
 
   const auto precompute_judge_data = [&out_bank]() -> void {
-    for (int g = 0; g < out_bank.num_words; g++) {
-      for (int t = 0; t < out_bank.num_words; t++) {
-        out_bank.verdicts[g][t] = judge(out_bank.words[g], out_bank.words[t]);
+    for (int i = 0; i < out_bank.num_words; i++) {
+      std::optional<int> sample_next_guesses[NUM_VERDICTS] = {};
+      for (int j = 0; j < out_bank.num_words; j++) {
+        int verdict = judge(out_bank.words[i], out_bank.words[j]);
+        out_bank.verdicts[i][j] = verdict;
+        sample_next_guesses[verdict] = j;
+      }
+      for (int candidate_verdict = 0; candidate_verdict < NUM_VERDICTS;
+           candidate_verdict++) {
+        if (!sample_next_guesses[candidate_verdict].has_value()) {
+          continue;
+        }
+        for (int prev_verdict = 0; prev_verdict < NUM_VERDICTS;
+             prev_verdict++) {
+          if (!sample_next_guesses[candidate_verdict].has_value()) {
+            continue;
+          }
+          char* candidate_guess =
+              out_bank.words[sample_next_guesses[candidate_verdict].value()];
+          int valid = check_is_hard_mode_valid(out_bank.words[i], prev_verdict,
+                                               candidate_guess);
+          out_bank
+              .hard_mode_valid_candidates[i][candidate_verdict][prev_verdict] =
+              valid;
+        }
       }
     }
   };
--- bot/main.cc
+++ bot/main.cc
@@ -5,13 +5,20 @@
 int main() {
   static wordy_witch::Bank bank;
   wordy_witch::load_bank(bank, "../bank/co_wordle",
-                         wordy_witch::BankGuessesInclusion::COMMON_WORDS);
+                         wordy_witch::BankGuessesInclusion::ALL_WORDS);
 
   std::cerr << bank.words[0] << " " << bank.words[bank.num_targets] << " "
             << bank.num_words << " " << bank.num_targets << std::endl;
-  int a = wordy_witch::find_word(bank, "stare").value();
-  int b = wordy_witch::find_word(bank, "steed").value();
+  int a = wordy_witch::find_word(bank, "sneer").value();
+  int b = wordy_witch::find_word(bank, "screw").value();
   std::cerr << bank.words[a] << " " << bank.words[b] << " "
             << wordy_witch::format_verdict(bank.verdicts[a][b]) << " "
             << wordy_witch::format_verdict(bank.verdicts[b][a]) << std::endl;
+  for (int v = 0; v < wordy_witch::NUM_VERDICTS; v++) {
+    std::cerr << v << " " << bank.words[a] << " " << bank.words[b] << " "
+              << wordy_witch::format_verdict(v) << " "
+              << bank.hard_mode_valid_candidates[a][bank.verdicts[a][b]][v] << " "
+              << bank.hard_mode_valid_candidates[b][bank.verdicts[b][a]][v]
+              << "\n";
+  }
 }
